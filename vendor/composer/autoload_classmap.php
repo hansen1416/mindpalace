68a8856083dc585cd0969d3236b92683a88fbc1c
hansen1416 <hansen1417@163.com>
2015-11-19 16:10:57 +0800

    laravel

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateCtgsTable' => $baseDir . '/database/migrations/2015_11_19_072350_create_ctgs_table.php',
+    'CreateItemsTable' => $baseDir . '/database/migrations/2015_11_19_072455_create_items_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
