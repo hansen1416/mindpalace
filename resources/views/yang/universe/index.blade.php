d326f0eb4ccc0ff179da73deb61a4e5e95202ec5
hansen1416 <hansen1417@163.com>
2016-04-11 17:04:34 +0800

    build shpere, elements faces not right, try to solve this before anything goes forward

--- resources/views/yang/universe/index.blade.php
+++ resources/views/yang/universe/index.blade.php
@@ -7,6 +7,10 @@
 <div class="axis y"><span class="d y">y</span></div>
 <div class="axis z"><span class="d z">z</span></div>
 
+<div class="plane x"></div>
+<div class="plane y"></div>
+<div class="plane z"></div>
+
 {!!$html!!}
 
 </div>
