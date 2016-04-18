a57769760135d3be8d760fd53971af66a5922347
hansen1416 <hansen1417@163.com>
2016-04-18 13:57:25 +0800

    try ro locate elements in each section

--- resources/assets/js/app/build/build.js
+++ resources/assets/js/app/build/build.js
@@ -18,14 +18,13 @@ define([
 
         Build.prototype.setup = function(confObj){
 
-			var THIS 		= this;
-            var stage       = document.getElementById('stage');
-			var common_clas = '.star';
-            var start_tier  = 0;
-            var R           = 200;           //每层球面实际半径
-            var gap         = 80;            //每一层球面的间隔
-            var a           = [];
-            var pos2        = [];
+            var THIS       = this;
+            var stage      = document.getElementById('stage');
+            var start_tier = 0;
+            var R          = 200;           //每层球面实际半径
+            var gap        = 80;            //每一层球面的间隔
+            var pos1       = [];
+            var pos2       = [];
 
         	(function init(){
 
@@ -34,9 +33,6 @@ define([
                     THIS.config[property] = confObj[property];
                 }
 
-                if (THIS.config.common_clas !== undefined) {
-                	common_clas = THIS.config.common_clas;
-                }
                 //在空间中定位元素
                 fibonacciShpere();
 
@@ -56,14 +52,9 @@ define([
 
                 var stars = stage.querySelectorAll('.tier-' + start_tier);
                 var N     = stars.length;
-                
+
                 //如果没有下一层了，则停止
                 if (!N) {return false;}
-
-                var dlong = Math.PI*(3-Math.sqrt(5));  /* ~2.39996323 */
-                var dz    = 2.0/N;
-                var long  = 0;
-                var z     = 1 - dz/2;
 
                 var tx = 0;     //X方向的位移
                 var ty = 0;     //Y方向的位移
@@ -75,40 +66,63 @@ define([
 
                 //当前层的半径
                 R += gap * start_tier;
+                pos1 = [];
+                //code from
+                //http://web.archive.org/web/20120421191837/http://www.cgafaq.info/wiki/Evenly_distributed_points_on_sphere
+                //positioning the points by spiral Fibonacci method
+                /* ~2.39996323 */
+                var dlong = Math.PI * (3 - Math.sqrt(5));
+                var dz    = 2.0 / N;
+                var long  = 0;
+                var z     = 1 - dz / 2;
+                var r     = 0;
 
                 for (var k = 0; k < N; k++){
-                    //code from 
-                    //http://web.archive.org/web/20120421191837/http://www.cgafaq.info/wiki/Evenly_distributed_points_on_sphere
-                    //positioning the points by spiral Fibonacci method
-                    r    = Math.sqrt(1-z*z);
-                    a[k] = {x:Math.cos(long)*r, y:Math.sin(long)*r, z:z};
+
+                    r    = Math.sqrt(1 - z * z);
+                    tx   = Math.cos(long) * r * R;
+                    ty   = Math.sin(long) * r * R;
+                    tz   = z * R;
                     z    = z - dz;
                     long = long + dlong;
                     //如果不是 DOM 对象，则跳出当前 for 循环
                     if (typeof stars[k] !== 'object') {break;}
 
-                    tx = a[k]['x'];
-                    ty = a[k]['y'];
-                    tz = a[k]['z'];
                     //判断元素是在z轴正方向还是负方向
                     sz = tz / Math.abs(tz);
                     sz = isNaN(sz) ? 1 : sz;
                     //如果是在Z轴正方向，
-                    // 则把元素沿y轴多旋转180度，是的正面朝向圆心
+                    //则把元素沿y轴多旋转180度，使得正面朝向圆心
                     if (sz > 0){
                         ry = Math.atan(tx/tz) + Math.PI;
                     }else{
                         ry = Math.atan(tx/tz);
                     }
 
-                    rx = Math.asin(ty);
+                    rx = Math.asin(ty/R);
 
-                    stars[k].style[prefixJs+"Transform"] =
-                            "translate3d("+ tx * R +"px, "+ ty * R +"px, "+ tz * R +"px)" +
+                    if (!start_tier) {
+                        stars[k].style[prefixJs+"Transform"] =
+                            "translate3d("+ tx +"px, "+ ty +"px, "+ tz +"px)" +
                             "rotateY("+ ry +"rad)" +
                             "rotateX("+ rx +"rad)";
 
+                        pos2[stars[k].dataset.id] = {tx:tx, ty:ty, tz:tz, ry:ry, rx:rx};
+                    }else{
+                        pos1[k] = {tx:tx, ty:ty, tz:tz, ry:ry, rx:rx};
+                    }
+
                 }
+
+                if (start_tier) {
+                    for (var i = 0; i < N; i++) {
+                        stars[i].style[prefixJs+"Transform"] =
+                            "translate3d("+ pos1[i]['tx'] +"px, "+ pos1[i]['ty'] +"px, "+ pos1[i]['tz'] +"px)" +
+                            "rotateY("+ pos1[i]['ry'] +"rad)" +
+                            "rotateX("+ pos1[i]['rx'] +"rad)";
+                    }
+                }
+
                 //深入到下一层
                 start_tier++;
 
