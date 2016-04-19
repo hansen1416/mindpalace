d1fea352fa7d5ef16ab97c7a3b7e4948e7c898d1
hansen1416 <hansen1417@163.com>
2016-04-19 18:19:03 +0800

    sphere scetion built

--- resources/assets/js/app/build/build.js
+++ resources/assets/js/app/build/build.js
@@ -23,7 +23,9 @@ define([
                 R        = 200,         //每层球面实际半径
                 gap      = 100,         //每一层球面的间隔
                 N        = 0,           //每一层球面上均匀分布的点的数量，不小于该层的元素数量
-                tierPos  = allPos = secColor = [];
+                allPos   = [],
+                tierPos  = [],
+                savedPos = [];
 
         	(function init(){
 
@@ -41,9 +43,9 @@ define([
                 }
 
                 //在空间中定位元素
-                fibonacciShpere();
+                diffuse();
 
-                tierPos = allPos = null;
+                tierPos = allPos = savedPos = null;
         	})();
 
             /**
@@ -54,7 +56,7 @@ define([
              * 通过 rotate3d 使得面和球面相切，并且转动到和纬线相平行的角度]
              * @author Hanlongzhen 2016-04-19 10:39
              */
-            function fibonacciShpere() {
+            function diffuse() {
 
                 var stars = stage.querySelectorAll('.tier-' + prevTier);
                 //如果没有下一层了，则停止
@@ -65,64 +67,138 @@ define([
                  * 如果不是，则先算出该层的上一层的分类数，记为 elders，再计算哪个父分类中的子分类最多，记为 sons，
                  * 然后取 elders*sons，stars.length，N 中的最大者，作为该层球面包含的点的数量
                  */
-                if (prevTier) {
-                    /**
-                     * pidKeys 储存父级分类的 id
-                     * pidValues 储存每一个父级分类包含的子分类的个数
-                     * j 表示 pidValues 的键名
-                     */
-                    var pidKeys   = [],
-                        pidValues = [],
-                        j         = -1;
+                N = prevTier ? maxPoint(stars, N) : stars.length;
 
-                    for (var i = 0; i < stars.length; i++) {
-
-                        if (!(stars[i] instanceof Object)) {
-                            continue;
-                        }
-                        var pid = stars[i].dataset.pid;
-
-                        if (pidKeys[pid] === undefined) {
-
-                            j++;
-                            pidKeys[pid] = true;
-                            pidValues[j] = 1;
-                        }else{
-                            pidValues[j]++;
-                        }
-
-                    }
-
-                    var elders = pidValues.length,
-                        sons = Math.max(...pidValues);
-
-                    pidKeys = pidValues = null;
-
-                    N = Math.max(elders * sons, stars.length, N);
-
-                }else{
-                    N = stars.length;
-                }
-
+                console.log(N, stars.length);
                 //当前层的半径
                 R += gap;
-                //初始化位置和旋转数组
-                tierPos = [];
 
-                //code from
-                //http://web.archive.org/web/20120421191837/http://www.cgafaq.info/wiki/Evenly_distributed_points_on_sphere
-                //positioning the points by spiral Fibonacci method
+                if (N == savedPos.length) {
+                    tierPos = savedPos;
+                }else {
 
+                    tierPos = savedPos = fibonacciShpere(N, R);
+                }
+
+                /**
+                 * 如果是最内层，则直接给元素定位，不需要考虑上级分类元素的位置
+                 * 如果不是最内层，则先把这一层所有元素的位置和旋转信息储存起来，
+                 * 再根据父分类的位置，计算元素所在位置，需要多一次循环
+                 * 在最内层中给每一个元素赋予一个背景颜色，他所有的后代分类全部继承此颜色
+                 * 颜色从预定义的颜色数组 colorCircle 中循环获取
+                 */
+                for (var i = 0; i < stars.length; i++) {
+                    //如果不是 DOM 对象，则跳出当前 for 循环
+                    if (!(stars[i] instanceof Object)) {
+                        continue;
+                    }
+
+                    var tx = ty = tz = ry = rx = 0;
+
+                    if (prevTier) {
+
+                        var p = allPos[stars[i].dataset.pid],
+                            k = closestPoint(p, tierPos);
+
+                            tx = tierPos[k]['tx'];
+                            ty = tierPos[k]['ty'];
+                            tz = tierPos[k]['tz'];
+                            ry = tierPos[k]['ry'];
+                            rx = tierPos[k]['rx'];
+                        
+                        stars[i].style[trsfm] =
+                            "translate3d("+ tierPos[k]['tx'] +"px, "+ tierPos[k]['ty'] +"px, "+ tierPos[k]['tz'] +"px)" +
+                            "rotateY("+ tierPos[k]['ry'] +"rad)" +
+                            "rotateX("+ tierPos[k]['rx'] +"rad)";
+
+                        stars[i].style['backgroundColor'] = p.c;
+
+                        allPos[stars[i].dataset.id] = {x:tx, y:ty, z:tz, c:p.c};
+
+                        tierPos.splice(k, 1);
+
+                    }else{
+
+                        var clr = colorCircle[i % colorCircle.length];
+                            tx  = tierPos[i]['tx'];
+                            ty  = tierPos[i]['ty'];
+                            tz  = tierPos[i]['tz'];
+                            ry  = tierPos[i]['ry'];
+                            rx  = tierPos[i]['rx'];
+
+                        allPos[stars[i].dataset.id] = {x:tx, y:ty, z:tz, c:clr};
+
+                        stars[i].style[trsfm] =
+                            "translate3d(" + tx + "px, " + ty + "px, " + tz + "px)" +
+                            "rotateY(" + ry + "rad)" +
+                            "rotateX(" + rx + "rad)";
+
+                        stars[i].style['backgroundColor'] = clr;
+
+                    }
+                }
+
+                //深入到下一层
+                prevTier++;
+
+                diffuse();
+            }
+
+            /**
+             * 则先算出该层的上一层的分类数，记为 elders，再计算哪个父分类中的子分类最多，记为 sons，
+             * 然后取 elders*sons，arr.length，n 中的最大者，作为该层球面包含的点的数量
+             * @param arr
+             * @param n
+             * @returns {number}
+             */
+            function maxPoint(arr, n) {
+                /**
+                 * keys 储存父级分类的 id
+                 * values 储存每一个父级分类包含的子分类的个数
+                 * j 表示 values 的键名
+                 */
+                var keys   = [],
+                    values = [],
+                    j      = -1;
+
+                for (var i = 0; i < arr.length; i++) {
+
+                    if (!(arr[i] instanceof Object)) {
+                        continue;
+                    }
+                    var pid = arr[i].dataset.pid;
+
+                    if (keys[pid] === undefined) {
+
+                        j++;
+                        keys[pid] = true;
+                        values[j] = 1;
+                    }else{
+                        values[j]++;
+                    }
+
+                }
+
+                var f = values.length,
+                    m = Math.max(...values);
+
+                return Math.max(f * m, arr.length, n);
+            }
+
+            //code from
+            //http://web.archive.org/web/20120421191837/http://www.cgafaq.info/wiki/Evenly_distributed_points_on_sphere
+            //positioning the points by spiral Fibonacci method
+            function fibonacciShpere(N, R) {
                 var dlong = Math.PI * (3 - Math.sqrt(5)),  // ~2.39996323
                     dz    = 2.0 / N,
                     long  = 0,
                     z     = 1 - dz / 2,
                     r     = 0;
+                //X方向的位移，Y方向的位移，Z方向的位移，X轴的旋转，Y轴的旋转，Z位移的正负号
+                var tx = ty = tz = rx = ry = sz = 0,
+                    arr = [];
 
                 for (var i = 0; i < N; i++) {
-
-                    //X方向的位移，Y方向的位移，Z方向的位移，X轴的旋转，Y轴的旋转，Z位移的正负号
-                    var tx = ty = tz = rx = ry = sz =0;
 
                     r    = Math.sqrt(1 - z * z);
                     tx   = Math.cos(long) * r * R;
@@ -136,81 +212,23 @@ define([
                     sz = isNaN(sz) ? 1 : sz;
                     //如果是在Z轴正方向，
                     //则把元素沿y轴多旋转180度，使得正面朝向圆心
-                    if (sz > 0){
-                        ry = Math.atan(tx/tz) + Math.PI;
-                    }else{
-                        ry = Math.atan(tx/tz);
+                    if (sz > 0) {
+                        ry = Math.atan(tx / tz) + Math.PI;
+                    } else {
+                        ry = Math.atan(tx / tz);
                     }
 
-                    rx = Math.asin(ty/R);
+                    rx = Math.asin(ty / R);
 
-                    /**
-                     * 如果是最内层，则直接给元素定位，不需要考虑上级分类元素的位置
-                     * 如果不是最内层，则先把这一层所有元素的位置和旋转信息储存起来，
-                     * 再根据父分类的位置，计算元素所在位置，需要多一次循环
-                     * 在最内层中给每一个元素赋予一个背景颜色，他所有的后代分类全部继承此颜色
-                     * 颜色从预定义的颜色数组 colorCircle 中循环获取
-                     */
-                    if (prevTier) {
-
-                        tierPos[i] = {tx:tx, ty:ty, tz:tz, ry:ry, rx:rx};
-                    }else{
-                        //如果不是 DOM 对象，则跳出当前 for 循环
-                        if (!(stars[i] instanceof Object)) {
-                            continue;
-                        }
-
-                        var clr = colorCircle[i % colorCircle.length];
-
-                        allPos[stars[i].dataset.id] = {x:tx, y:ty, z:tz, c:clr};
-
-                        stars[i].style[trsfm] =
-                            "translate3d("+ tx +"px, "+ ty +"px, "+ tz +"px)" +
-                            "rotateY("+ ry +"rad)" +
-                            "rotateX("+ rx +"rad)";
-
-                        stars[i].style['backgroundColor'] = clr;
-                    }
+                    arr[i] = {tx: tx, ty: ty, tz: tz, ry: ry, rx: rx};
 
                 }
 
-                /**
-                 * 对于非最内层，先获取元素父级分类的位置，
-                 * 然后选出当前层中离父级位置最近的位置，给本层元素定位和旋转
-                 * 从父级中获取到颜色，赋予当前元素
-                 */
-                if (prevTier) {
-                    for (var i = 0; i < stars.length; i++) {
-                        //如果不是 DOM 对象，则跳出当前 for 循环
-                        if (!(stars[i] instanceof Object)) {
-                            continue;
-                        }
-
-                        var p = allPos[stars[i].dataset.pid],
-                            k = closestPoint(p, tierPos);
-
-                        stars[i].style[trsfm] =
-                            "translate3d("+ tierPos[k]['tx'] +"px, "+ tierPos[k]['ty'] +"px, "+ tierPos[k]['tz'] +"px)" +
-                            "rotateY("+ tierPos[k]['ry'] +"rad)" +
-                            "rotateX("+ tierPos[k]['rx'] +"rad)";
-
-                        stars[i].style['backgroundColor'] = p.c;
-
-                        allPos[stars[i].dataset.id] = {x:tx, y:ty, z:tz, c:p.c};
-
-                        tierPos.splice(k, 1);
-                    }
-                }
-
-                //深入到下一层
-                prevTier++;
-
-                fibonacciShpere();
+                return arr;
             }
-
 
         }
 
         window.Build = Build;
 
-})
+});
