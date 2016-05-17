f172fa220cf39e6fb917c7be10b903500da001a5
hansen1416 <hansen1417@163.com>
2016-05-17 14:48:52 +0800

    制作 #operation.  定位在球心位置

--- resources/assets/js/app/build/func/conceal.js
+++ resources/assets/js/app/build/func/conceal.js
@@ -1,5 +1,7 @@
 define(function() {
-
+    /**
+     * 隐藏元素
+     */
     return function (element){
         element.style['display'] = 'none';
     }
--- resources/assets/js/app/build/func/reveal.js
+++ resources/assets/js/app/build/func/reveal.js
@@ -1,5 +1,7 @@
 define(function() {
-
+    /**
+     * 显示元素
+     */
     return function (element){
         element.style['display'] = 'block';
     }
