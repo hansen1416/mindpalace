82a4524672e670abe62da11d24a3cfa9f49cd013
hansen1416 <hansen1417@163.com>
2016-06-01 18:13:14 +0800

    制作用户系统

--- resources/views/layouts/yang/home.blade.php
+++ resources/views/layouts/yang/home.blade.php
@@ -9,8 +9,8 @@
     <title>{{ trans('general.pageTitle') }}</title>
 
     @if(App::environment('development'))
-        <link rel="stylesheet/less" type="text/css" href="../../../assets/less/yang.less">
-        <script src="./resources/assets/less/less.min.js"></script>
+        <link rel="stylesheet/less" type="text/css" href="/resources/assets/less/yang.less">
+        <script src="/resources/assets/less/less.min.js"></script>
     @else
         <link href="{{ URL::asset('/css/home.css') }}" rel="stylesheet">
         <script src="{{ URL::asset('/js/require.js') }}"></script>
