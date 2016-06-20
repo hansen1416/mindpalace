cba2925616969503463ccfc4cdaf160fe3b17d82
hansen1416 <hansen1417@163.com>
2016-06-20 18:10:59 +0800

    改版，使用 ECMA6 class 关键字定义类，重构完成

--- resources/assets/js/yang/yang-home.js
+++ resources/assets/js/yang/yang-home.js
@@ -14,21 +14,3 @@ define([
 
 
 });
-
-//define([
-//        "./var/document",
-//        './build/build_home',
-//
-//   ], function (document, build_home) {
-//
-//
-//    var B = new BuildHome({
-//        'annu': [
-//            {'selector': '.panel', 'radius': 150},
-//        ],
-//    });
-//
-//    B.click();
-//
-//
-//});
