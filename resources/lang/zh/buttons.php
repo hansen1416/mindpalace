905f21993b7b4eb1e942d916c9738ae470ece284
hansen1416 <badapplesweetie@gmail.com>
2016-06-26 19:43:12 +0800

    在按钮中使用汉字

---
+++ resources/lang/zh/buttons.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'focus'     => 'focus the selected element',
+    'addSibl'   => 'Add a sibling category',
+    'addDesc'   => 'Add a descendant category',
+    'editSelf'  => 'Edit the title',
+    'hideOper'  => 'Hide the operation panel',
+    'addItem'   => 'Add a item',
+    'showItem'  => 'Display the item content',
+    'title'     => [
+        'focus'     => 'focus the selected element',
+        'addSibl'   => 'Add a sibling category',
+        'addDesc'   => 'Add a descendant category',
+        'editSelf'  => 'Edit the title',
+        'hideOper'  => 'Hide the operation panel',
+        'addItem'   => 'Add a item',
+        'showItem'  => 'Display the item content',
+    ]
+];
