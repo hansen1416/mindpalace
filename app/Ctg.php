d30b2fcfdcc5367b7eef590d54d982f84f931648
hansen1416 <hansen1417@163.com>
2016-07-06 14:59:33 +0800

    美化，尝试水墨风格，星云的配色

--- app/Ctg.php
+++ app/Ctg.php
@@ -102,8 +102,7 @@ class Ctg extends Model {
     /**
      * 将所有分类用 html 标签包裹，赋予相应的 class 和 dataset
      * @param $array
-     *
-     * @return string
+     * @return html
      */
     public function tagWrap($array)
     {
@@ -123,23 +122,44 @@ class Ctg extends Model {
             if ($value->tier > $start) break;
         }
 
+        /**
+         * $core_id_flip 代表每一个最内层分类对应的 section
+         * 从 0 - 9 大于9的取 10 的余数
+         * 他的子集元素全部继承最内层的 section
+         * .sec-0, .sec-1 ... .sec-9
+         */
         $core_id_flip = array_flip($core_id);
 
         foreach ($array as $value) {
 
+            /**
+             * $start 可能是 0 也可能不是
+             * $tier 是相对于最内层的层数
+             */
             $tier = $value->tier - $start;
-            //在其后的子元素中寻找他属于最内层的哪一个分类，咋把它归到哪一个 section 中
+
+            /**
+             * 在其后的子元素中寻找他属于最内层的哪一个分类
+             * 继承最内层的 section
+             */
             if ($tier) {
                 preg_match('/^-('.join('|', $core_id).')-/', $value->path, $match);
-                if ($match && isset($match[1])) $section = 'sec-' . $core_id_flip[$match[1]];
+                if ($match && isset($match[1])) $section = 'sec-' . $core_id_flip[$match[1]] % 10;
             }else{
-                $section = 'sec-' . $core_id_flip[$value->ctg_id];
+                $section = 'sec-' . $core_id_flip[$value->ctg_id] % 10;
             }
 
+            /**
+             * 组合分类的 html
+             */
             $html .= "<div class='tier-{$tier} star {$section} ctg' title='{$value->title}' data-title='{$value->title}' data-ctg_id='{$value->ctg_id}' data-pid='{$value->pid}' data-tier='{$tier}' data-sort='{$value->sort}'>" .
                     $value->title .
                     "</div>";
 
+            /**
+             * 如果有内容元素
+             * 拼接内容元素的 html
+             */
             if ( count($value->item) ) {
                 $tier = $tier + 1;
                 foreach ($value->item as $item) {
@@ -148,6 +168,7 @@ class Ctg extends Model {
                              "</div>";
                 }
             }
+
         }
 
         return $html;
