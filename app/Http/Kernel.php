eb5f128b437927379414ab72ff1f401a74557e63
hansen1416 <hansen1417@163.com>
2016-07-07 12:05:53 +0800

    理解 laravel 结构，添加注释

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,7 +10,8 @@ class Kernel extends HttpKernel
      * The application's global HTTP middleware stack.
      *
      * These middleware are run during every request to your application.
-     *
+     * 全局中间件
+     * 每一个请求都会经过此中间件
      * @var array
      */
     protected $middleware = [
@@ -19,7 +20,8 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware groups.
-     *
+     * 中间件组，只是让一次分配给路由多个中间件的实现更加简单
+     * 通过 ['middleware' => ['web']] 来给路由或路由组制定中间件组
      * @var array
      */
     protected $middlewareGroups = [
@@ -40,7 +42,8 @@ class Kernel extends HttpKernel
      * The application's route middleware.
      *
      * These middleware may be assigned to groups or used individually.
-     *
+     * 应用到单个或一组路由中
+     * 在路由中通过 ['middleware' => 'auth', ...]来指定这里定义的中间件
      * @var array
      */
     protected $routeMiddleware = [
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Http\Middleware;
+
 use Closure;
 use Illuminate\Support\Facades\Auth;
+
 class Authenticate
 {
     /**
@@ -9,7 +11,7 @@ class Authenticate
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param  string|null  $guard  中间件参数可以在定义路由时通过：分隔中间件名和参数名来指定，多个中间件参数可以通过逗号分隔
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
@@ -21,6 +23,11 @@ class Authenticate
                 return redirect()->route('home');
             }
         }
+
+        /**
+         * 在 HTTP 请求处理之前，
+         * 做了以上处理
+         */
         return $next($request);
     }
 }
