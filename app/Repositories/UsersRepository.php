26e253139ccbabdc6d74df3c9c45a348646117cf
hansen1416 <hansen1417@163.com>
2016-07-08 16:31:52 +0800

    理解 laravel 结构，添加注释，加入仓库

--- app/Repositories/UsersRepository.php
+++ app/Repositories/UsersRepository.php
@@ -4,6 +4,12 @@ namespace App\Repositories;
 use App\User;
 use App\Profile;
 
+/**
+ * 用于从模型获取所有的用户相关信息
+ * 可以统一管理查询语言
+ * Class UsersRepository
+ * @package App\Repositories
+ */
 class UsersRepository
 {
 
