132d481e19526061b56f3bd621f83dad53340216
hansen1416 <hansen1417@163.com>
2016-07-12 12:02:15 +0800

    剥离事件部分，重组 js 结构

--- resources/assets/js/yang/yang-space.js
+++ resources/assets/js/yang/yang-space.js
@@ -1,9 +1,9 @@
 define([
 
-    "./class/layout/yang_space_layout",
-    "./class/event/yang_space_event"
+           "./class/layout/yang_space_layout",
+           "./class/event/yang_space_event"
 
-], function (yang_space_layout, yang_space_event) {
+       ], function (yang_space_layout, yang_space_event) {
 
     /**
      * 引入 class YangSpaceLayout
@@ -18,9 +18,9 @@ define([
      * 所有按钮环形排布
      */
     window.YangSpaceLayout.setOperation([
-                                      {'selector': '.ctg_btn', 'radius': 100},
-                                      {'selector': '.item_btn', 'radius': 100}
-                                  ]);
+                                            {'selector': '.ctg_btn', 'radius': 100},
+                                            {'selector': '.item_btn', 'radius': 100}
+                                        ]);
 
     var ysl = new YangSpaceLayout({
         stage    : document.getElementById('stage'),
@@ -32,19 +32,19 @@ define([
     //生成同心球面
     ysl.spheres();
 
+    var yse = new YangSpaceEvent();
 
 
-    //ys.trackball({
-    //                 //true有惯性，false没有惯性
-    //                 impulse    : true,
-    //                 //当鼠标点击目标元素时，是否停止当前运动，true为停止
-    //                 resetMotion: true,
-    //                 //单位角速度的cap,必须是大于0的数，默认为0.5
-    //                 omegaGap   : 0.8,
-    //                 //阻力系数，越大阻力越大，默认0.01
-    //                 lambda     : 0.001
-    //             });
-
+    yse.trackball({
+                     //true有惯性，false没有惯性
+                     impulse    : true,
+                     //当鼠标点击目标元素时，是否停止当前运动，true为停止
+                     resetMotion: true,
+                     //单位角速度的cap,必须是大于0的数，默认为0.5
+                     omegaGap   : 0.8,
+                     //阻力系数，越大阻力越大，默认0.01
+                     lambda     : 0.001
+                 });
 
 
 });
