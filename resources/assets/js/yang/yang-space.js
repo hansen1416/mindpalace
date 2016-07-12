d36cc47969ae38cf7042befc9ab95cbf24c93357
hansen1416 <hansen1417@163.com>
2016-07-12 15:18:35 +0800

    剥离事件部分，重组 js 结构

--- resources/assets/js/yang/yang-space.js
+++ resources/assets/js/yang/yang-space.js
@@ -36,15 +36,15 @@ define([
      * 3d 球体转动
      */
     Y.trackball({
-                      //true有惯性，false没有惯性
-                      impulse    : true,
-                      //当鼠标点击目标元素时，是否停止当前运动，true为停止
-                      resetMotion: true,
-                      //单位角速度的cap,必须是大于0的数，默认为0.5
-                      omegaGap   : 0.8,
-                      //阻力系数，越大阻力越大，默认0.01
-                      lambda     : 0.001
-                  });
+                    //true有惯性，false没有惯性
+                    impulse    : true,
+                    //当鼠标点击目标元素时，是否停止当前运动，true为停止
+                    resetMotion: true,
+                    //单位角速度的cap,必须是大于0的数，默认为0.5
+                    omegaGap   : 0.8,
+                    //阻力系数，越大阻力越大，默认0.01
+                    lambda     : 0.001
+                });
 
     /**
      * 鼠标滚轮事件
