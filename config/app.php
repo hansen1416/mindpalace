f1d2e8a8708f829e18554da4a84dfdde1e435d06
hansen1416 <hansen1417@163.com>
2016-07-15 11:42:11 +0800

    添加 laravel-IDE-helper

--- config/app.php
+++ config/app.php
@@ -141,10 +141,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        /**
-         * IDE completion
-         */
-//        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
 	],
 
