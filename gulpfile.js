f6654e45e3e132c99c25e293b28a7adf6ed071e7
hansen1416 <badapplesweetie@gmail.com>
2016-07-17 18:20:54 +0800

    压缩 js 和 css

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,50 @@
-var gulp    = require('gulp'),
-    webpack = require('webpack-stream'),
-    less    = require('gulp-less'),
-    babel   = require('gulp-babel-compile'),
-    uglify  = require('gulp-uglify'),
-    assets  = './resources/assets/';
+var gulp     = require('gulp'),
+    pump     = require('pump'),
+    webpack  = require('webpack-stream'),
+    minifier = require('gulp-uglify/minifier'),
+    harmony  = require('uglify-js-harmony'),
+    options  = {},
+    less     = require('gulp-less'),
+    assets   = './resources/assets/';
 
-gulp.task('webpack', function () {
+gulp.task('webpack_home', function (cb) {
     "use strict";
 
-    gulp.src([assets + 'js/yang/yang-home.js'])
-        .pipe(webpack({
-                          entry : './resources/assets/js/yang/yang-home.js',
-                          output: {
-                              filename: 'yang-home.js'
-                          }
-                      }))
-        .pipe(gulp.dest('public/js'))
-        .pipe(babel({
-                        presets: ['es2015']
-                    }))
-        .pipe(gulp.dest('public/js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+    pump([
+             gulp.src(assets + 'js/yang/yang-home.js'),
+             webpack({
+                         entry : './resources/assets/js/yang/yang-home.js',
+                         output: {
+                             filename: 'yang-home.js'
+                         }
+                     }),
+             gulp.dest('public/js'),
+             minifier(options, harmony),
+             gulp.dest('public/js')
+         ],
+         cb
+    );
 
+});
 
+gulp.task('webpack_space', function (cb) {
+    "use strict";
 
-    gulp.src(assets + 'js/yang/yang-space.js')
-        .pipe(webpack({
-                          entry : './resources/assets/js/yang/yang-space.js',
-                          output: {
-                              filename: 'yang-space.js'
-                          }
-                      }))
-        .pipe(gulp.dest('public/js'))
-        .pipe(babel({
-                        presets: ['es2015']
-                    }))
-        .pipe(gulp.dest('public/js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+    pump([
+             gulp.src(assets + 'js/yang/yang-space.js'),
+             webpack({
+                         entry : './resources/assets/js/yang/yang-space.js',
+                         output: {
+                             filename: 'yang-space.js'
+                         }
+                     }),
+             gulp.dest('public/js'),
+             minifier(options, harmony),
+             gulp.dest('public/js')
+         ],
+         cb
+    );
+
 
 });
 
@@ -62,8 +68,9 @@ gulp.task('less', function () {
 
 
 gulp.task('default', function () {
-    gulp.run('webpack');
-    gulp.run('less');
+    gulp.run('webpack_home');
+    gulp.run('webpack_space');
+    //gulp.run('less');
 });
 
 
