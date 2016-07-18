aa09390838bb68b5f9016d44008c09445f041749
hansen1416 <badapplesweetie@gmail.com>
2016-07-18 23:14:17 +0800

    1

--- resources/assets/js/yang/func/math/matrixToArr.js
+++ resources/assets/js/yang/func/math/matrixToArr.js
@@ -8,6 +8,11 @@ define(function() {
 	return function (matrix){
 
         var arr = matrix.split(",");
+
+        if (arr.length < 16) {
+            return new Float32Array([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]);
+        }
+
         arr[0]  = arr[0].replace(/(matrix3d\()/g, "");
         arr[15] = arr[15].replace(/\)/g, "");
 
