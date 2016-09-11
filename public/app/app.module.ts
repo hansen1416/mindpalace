8ffc754859bc3b415577385a7eefd565212644f8
hlz <hansen1417@163.com>
2016-09-11 22:00:04 +0800

    angularJs

---
+++ public/app/app.module.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by hlz on 16-9-11.
+ */
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports:      [ BrowserModule ]
+})
+export class AppModule { }
