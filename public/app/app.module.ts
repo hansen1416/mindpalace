4b9dfc3105b836324a0800d2aaef670614c91004
hlz <hansen1417@163.com>
2016-09-12 23:22:02 +0800

    angularJs

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,10 +1,9 @@
-/**
- * Created by hlz on 16-9-11.
- */
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppComponent }   from './app.component';
 @NgModule({
-    imports:      [ BrowserModule ]
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ public/app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by hlz on 16-9-12.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
