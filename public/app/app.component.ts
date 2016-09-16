86a7a9f0535b9398d7f1ab2744af4db9c7e9f8b4
hlz <hansen1417@163.com>
2016-09-16 23:40:28 +0800

    Angular2 get started, for our dream

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,9 +1,17 @@
 import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template: `<h1>{{title}}</h1>
+                    <h2>My favorite hero is: {{myHero}}</h2>
+                <p>Heroes:</p>
+                <ul>
+                    <li *ngFor="let hero of heroes">
+                    {{ hero }}
+                </li>
+                </ul>`
 })
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero = 'Windstorm';
+    heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+    myHero = this.heroes[0];
 }
