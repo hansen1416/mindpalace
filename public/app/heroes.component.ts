e02dcc171ef512be95dcd2cd70f8a402a281b240
hlz <hansen1417@163.com>
2016-09-25 23:45:18 +0800

    Angular2 get started, for our dream

--- public/app/heroes.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component }          from '@angular/core';
-@Component({
-    selector: 'my-heroes',
-    template: `
-  <h2>Heroes</h2>
-  <hero-list></hero-list>
-  `
-})
-export class HeroesComponent { }
