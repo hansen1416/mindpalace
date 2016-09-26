51a6585766dcdd19156cec4cf72f8788c0f046a4
hlz <hansen1417@163.com>
2016-09-26 22:22:02 +0800

    Angular2 get started, for our dream

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,28 +1,22 @@
 import {Component} from '@angular/core';
-export class Hero {
-    id: number;
-    name: string;
-}
+
+import {Hero} from './hero';
+
 @Component({
-               selector: 'my-app',
-               template: `
-                        <h1>{{title}}</h1>
-                        <h2>My Heroes</h2>
-                        <ul class="heroes">
-                            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
-                                <span class="badge">{{hero.id}}</span>{{hero.name}}
-                            </li>
-                        </ul>
-                        <div *ngIf="selectedHero">
-                        <h2>{{selectedHero.name}} details!</h2>
-                        <div><label>id: </label>{{selectedHero.id}}</div>
-                        <div>
-                            <label>name: </label>
-                            <input [(ngModel)]="selectedHero.name" placeholder="name">
-                        </div>
-                        </div>
-                    `,
-               styles  : [`
+    selector: 'my-app',
+    template: `
+                <h1>{{title}}</h1>
+                <h2>My Heroes</h2>
+                <ul class="heroes">
+                    <li *ngFor="let hero of heroes" 
+                    [class.selected]="hero === selectedHero"
+                    (click)="onSelect(hero)">
+                        <span class="badge">{{hero.id}}</span>{{hero.name}}
+                    </li>
+                </ul>
+                <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+                `,
+    styles: [`
                       .selected {
                         background-color: #CFD8DC !important;
                         color: white;
@@ -71,12 +65,13 @@ export class Hero {
                         border-radius: 4px 0 0 4px;
                       }
                 `]
-           })
+})
 export class AppComponent {
     title: string = 'Tour of Heroes';
     selectedHero: Hero;
-    heroes        = HEROES;
-    onSelect(hero: Hero): void{
+    heroes = HEROES;
+
+    onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
 }
