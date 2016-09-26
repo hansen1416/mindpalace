4a44b7ac152f89d4db81c23944da0324c9af86e6
hlz <hansen1417@163.com>
2016-09-27 00:06:39 +0800

    Angular2 get started, for our dream

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
+import {OnInit} from '@angular/core';
 
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -64,27 +66,29 @@ import {Hero} from './hero';
                         margin-right: .8em;
                         border-radius: 4px 0 0 4px;
                       }
-                `]
+                `],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+    constructor(private heroService: HeroService){
+
+    }
+
+    ngOnInit() :void{
+        this.getHeroes();
+    }
+
     title: string = 'Tour of Heroes';
     selectedHero: Hero;
-    heroes = HEROES;
+    heroes: Hero[];
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
-}
 
-const HEROES: Hero[] = [
-    {id: 11, name: 'Mr. Nice'},
-    {id: 12, name: 'Narco'},
-    {id: 13, name: 'Bombasto'},
-    {id: 14, name: 'Celeritas'},
-    {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
-];
+    getHeroes(): void{
+        // this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    }
+}
