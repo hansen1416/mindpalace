2a9c38e6521a390b56cabad9bd95d9d663a6563b
mok <hansen1417@163.com>
2016-09-27 17:19:48 +0800

    family dream focus

--- public/app/dashboard.component.ts
+++ public/app/dashboard.component.ts
@@ -1,11 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
-               selector: 'my-dashboard',
-               template: `
-                            <h3>My Dashboard</h3>
-                        `
+               moduleId   : module.id,
+               selector   : 'my-dashboard',
+               templateUrl: 'dashboard.component.html',
+               styleUrls  : ['dashboard.component.css']
            })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+    heroes: Hero[] = [];
+
+    constructor(private router: Router,
+                private heroService: HeroService) {
+    }
+
+    ngOnInit(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+
+    goToDetail(hero: Hero): void {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+    }
 }
