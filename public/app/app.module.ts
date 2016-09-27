711f6ef9ad4a627c467c4c1ed07b8407a5f6df9b
mok <hansen1417@163.com>
2016-09-27 15:14:55 +0800

    family dream focus

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -5,18 +5,24 @@ import {FormsModule}   from '@angular/forms';
 import {AppComponent}   from './app.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {DashboardComponent} from "./dashboard.component";
 
 import {HeroService} from './hero.service';
+
+import {routing} from './app.routing';
+
 
 @NgModule({
               imports     : [
                   BrowserModule,
-                  FormsModule
+                  FormsModule,
+                  routing
               ],
               declarations: [
                   AppComponent,
                   HeroesComponent,
                   HeroDetailComponent,
+                  DashboardComponent
               ],
               providers   : [
                   HeroService
