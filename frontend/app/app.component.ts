f39d98e20489132ecad5636a47b5ac97a153e929
mok <hansen1417@163.com>
2016-10-11 10:42:26 +0800

    keep going

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,6 +1,17 @@
 import {Component} from '@angular/core';
 
-@Component()
+@Component({
+               moduleId: module.id,
+               selector: 'my-app',
+               template: `
+                        <h1>{{title}}</h1>
+                        <nav>
+                            <a>Dashboard</a>
+                            <a>Heroes</a>
+                        </nav>
+                        <router-outlet></router-outlet>
+                    `
+           })
 export class AppComponent {
     title: string = 'Tour of Heroes';
 }
