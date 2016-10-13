4444385b3b4564c6a457bb40bf7b5c49858cb42a
hlz <hansen1417@163.com>
2016-10-13 23:31:49 +0800

    for our dream. keep going

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,34 +1,83 @@
-import {Component, OnInit} from '@angular/core';
-// import {UserService} from './user/user.service';
-// import {User} from './user/user';
+/*
+ * Angular 2 decorators and services
+ */
+import { Component, ViewEncapsulation } from '@angular/core';
 
+import { AppState } from './app.service';
+
+/*
+ * App Component
+ * Top Level Component
+ */
 @Component({
-               // moduleId   : module.id,
-               selector   : 'my-app',
-               templateUrl: 'app.component.html'
-           })
-export class AppComponent implements OnInit {
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.css'
+  ],
+  template: `
+    <nav>
+      <span>
+        <a [routerLink]=" ['./'] ">
+          Index
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./home'] ">
+          Home
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./detail'] ">
+          Detail
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./about'] ">
+          About
+        </a>
+      </span>
+    </nav>
 
-    constructor(
-        // private userService: UserService,
-        // private user: User
-    ) {
-    }
+    <main>
+      <router-outlet></router-outlet>
+    </main>
 
-    ngOnInit() {
-        // this.userService.authenticate().subscribe(
-        //     (response) => {
-        //         this.user = response;
-        //         console.log(this.user);
-        //
-        //         this.userService.ctg(this.user).subscribe(
-        //             (response) => {
-        //                 console.log(response);
-        //             }
-        //         );
-        //
-        //     }
-        // );
+    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
 
-    }
+    <footer>
+      <span>WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a></span>
+      <div>
+        <a [href]="url">
+          <img [src]="angularclassLogo" width="25%">
+        </a>
+      </div>
+    </footer>
+  `
+})
+export class App {
+  angularclassLogo = 'assets/img/angularclass-avatar.png';
+  name = 'Angular 2 Webpack Starter';
+  url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState) {
+
+  }
+
+  ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
 }
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
