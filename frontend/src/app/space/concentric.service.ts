4fa748e4d3a104baa2d19b228ce1b8a8d60ff3f3
mok <hansen1417@163.com>
2016-11-30 16:09:04 +0800

    building space home. method 2

--- frontend/src/app/space/concentric.service.ts
+++ frontend/src/app/space/concentric.service.ts
@@ -8,10 +8,58 @@ import {CssService} from '../share/css.service';
 @Injectable()
 export class ConcentricService {
 
+    //中心空白的宽度,unit rem
+    private a = 20;
+    //中心空白的高度,unit rem
+    private b = 15;
+
+    private k = 5;
+
+    private g = 3;
+
     constructor(
         private css: CssService
     ) {}
 
+
+    get innerWidth() {
+        return this.a;
+    }
+
+
+    set setInnerWidth(a) {
+        this.a = a;
+    }
+
+
+    get innerHeight() {
+        return this.b;
+    }
+
+
+    set setInnerHeight(b) {
+        this.b = b;
+    }
+
+
+    get itemWidth() {
+        return this.k;
+    }
+
+
+    get itemHeight() {
+        return this.g;
+    }
+
+
+    get horizontalNum() {
+        return Math.round(this.innerWidth / this.itemWidth);
+    }
+
+
+    get verticalNum(){
+        return Math.round(this.innerHeight / this.itemHeight);
+    }
 
     serConcentricCircles(selector: string): boolean {
 
@@ -30,10 +78,6 @@ export class ConcentricService {
             let x: number;
             //y轴位移
             let y: number;
-            //中心空白的宽度,unit rem
-            let a = 20;
-            //中心空白的高度,unit rem
-            let b = 15;
             //单个node占据的宽度, 包括margin
             let k = 5;
             //单个node占据的高度, 包括margin
@@ -45,8 +89,8 @@ export class ConcentricService {
 
             while (n < length) {
 
-                w = Math.round(a / k);
-                h = Math.round(b / g);
+                w = this.horizontalNum;
+                h = this.verticalNum;
 
                 let t = [
                     w - 1,
@@ -60,24 +104,25 @@ export class ConcentricService {
                  */
                 if (i <= t[0]) {
                     x = (i - w / 2 + 1) * k;
-                    y = b / -2;
+                    y = this.innerHeight / -2;
                     //上边
                 } else if (i > t[0] && i <= t[1]) {
-                    x = a / 2;
+                    x = this.innerWidth / 2;
                     y = ((i - t[0] - h / 2) * g);
                     //右边
                 } else if (i > t[1] && i <= t[2]) {
                     x = (w / 2 - (i - t[1])) * k;
-                    y = b / 2;
+                    y = this.innerHeight / 2;
                     //底边
                 } else if (i > t[2] && i <= t[3]) {
-                    x = a / -2;
+                    x = this.innerWidth / -2;
                     y = ((h / 2 - (i - t[2])) * g);
                     //左边
                 } else {
                     i = 0;
-                    a = a + k * 2;
-                    b = b + g * 2;
+
+                    this.setInnerWidth  = this.innerWidth + k * 2;
+                    this.setInnerHeight = this.innerHeight + g * 2;
                     continue;
                     /**
                      * 一圈转弯了,将中心空白扩大一圈开始转下一圈
@@ -87,7 +132,7 @@ export class ConcentricService {
                 /**
                  * 以顶部和左右为界,不可超出
                  */
-                if (Math.abs(x) * this.css.remPx * 2 > this.css.bw || (y < 0 && Math.abs(y) * this.css.remPx * 2 > this.css.bh) ) {
+                if (Math.abs(x) * this.css.remPx * 2 > this.css.bw || (y < 0 && Math.abs(y) * this.css.remPx * 2 > this.css.bh)) {
                     i++;
                     continue;
                 }
@@ -97,7 +142,7 @@ export class ConcentricService {
                                                "rotateX(" + 0 + "rad)";
                 i++;
                 n++;
-                
+
             }
 
             return true;
