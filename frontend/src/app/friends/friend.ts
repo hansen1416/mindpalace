76982d36bf589d293f158efa09b7344f3a8de7f1
hansen1416 <hansen1417@163.com>
2017-04-26 18:01:35 +0800

    master init

--- frontend/src/app/friends/friend.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hlz on 17-3-1.
- */
-export class Friend {
-    user_id: number;
-    name: string;
-    portrait: string;
-    is_friend: number;
-}
--- frontend/src/app/lang/lang-available.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by hlz on 17-2-20.
- */
-export const languages = [
-    {'key': 'zh', 'name': '中', 'fullName': '中文'},
-    {'key': 'en', 'name': 'EN', 'fullName': 'English'}
-];
