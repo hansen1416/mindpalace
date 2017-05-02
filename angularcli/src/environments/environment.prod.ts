407523e705b11f292282d1f9e167de7e9083fec5
hlz <hansen1417@163.com>
2017-05-02 22:04:04 +0800

    prepare for production

--- angularcli/src/environments/environment.prod.ts
+++ angularcli/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     ws        : 'ws://127.0.0.1:9501',
-    api       : 'http://api.mindpalaces.com/api/'
+    api       : 'http://api.minditate.com/api/'
 };
