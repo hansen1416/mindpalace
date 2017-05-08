587a658a1a1e23fbc1855c8c2d62fb3f06961ee1
hansen1416 <hansen1417@163.com>
2017-05-08 17:52:51 +0800

    declare help component in help module

---
+++ angularcli/src/app/help/help-routing.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by hlz on 17-5-8.
+ */
+import {NgModule}     from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+              imports: [],
+              exports: [
+                  RouterModule
+              ]
+          })
+export class HelpRoutingModule {
+}
